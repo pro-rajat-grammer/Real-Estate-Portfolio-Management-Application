cbc2518e340bb8b0e7604291c6a20d01e5fe01e4
Rajat Naroji <138668918+pro-rajat-grammer@users.noreply.github.com>
2024-02-24 15:45:20 +0530

    final commit

--- frontend/src/components/Profile.js
+++ frontend/src/components/Profile.js
@@ -1,13 +1,9 @@
-
 import React from 'react'
 
 const Profile = () => {
-
     const auth = localStorage.getItem("user")
-
   return (
     <div className='profile'>
-    
           <div >
           <h3>Your Profile Details</h3>
             <h3> Name = {JSON.parse(auth).name}</h3>
