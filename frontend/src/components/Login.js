a3a119d8dea19013de020a09f374714500de5403
Rajat Naroji <138668918+pro-rajat-grammer@users.noreply.github.com>
2024-02-24 15:42:44 +0530

    final commit

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -4,10 +4,9 @@ import React from 'react'
 import { useNavigate } from "react-router-dom"
 
 const Login = () => {
-
   const [email, setEmail] = useState("")
   const [password, setPassword] = useState("")
-
+  
   const navigate = useNavigate();
 
   useEffect(() => {
@@ -41,14 +40,12 @@ const Login = () => {
     <div className='login'>
       <img src="https://c.wallhere.com/photos/24/cc/city_urban_architecture_building_cityscape_reflection_lights_city_lights-1618577.jpg!d" alt="" />
       <h5> Real Estate Portfolio Management</h5>
-
       <form>
         <h3> Login </h3>
         <input type='text' placeholder='enter email' value={email} onChange={(e) => setEmail(e.target.value)} />
         <input type='password' placeholder='enter password' value={password} onChange={(e) => setPassword(e.target.value)} />
         <button onClick={collectData} type='button'> Signup</button>
       </form>
-
     </div>
   )
 }
