487da8582795d3cadb95a3a54d3e192868022848
Rajat Naroji <138668918+pro-rajat-grammer@users.noreply.github.com>
2024-02-24 15:43:12 +0530

    final commit

--- frontend/src/components/PrivateComponent.js
+++ frontend/src/components/PrivateComponent.js
@@ -1,13 +1,10 @@
 import {Navigate,Outlet} from "react-router-dom"
-
 import React from 'react'
 
 const PrivateComponent = () => {
-
     const auth = localStorage.getItem("user")
   return (
    auth ? <Outlet/> : <Navigate to = "/login" />
   )
 }
-
 export default PrivateComponent
