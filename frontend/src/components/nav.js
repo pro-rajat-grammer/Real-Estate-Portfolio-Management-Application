77df4fd81868b51eb34f62e0470c97e09971e5e9
Rajat Naroji <138668918+pro-rajat-grammer@users.noreply.github.com>
2024-02-24 15:47:01 +0530

    final commit

--- frontend/src/components/nav.js
+++ frontend/src/components/nav.js
@@ -1,11 +1,7 @@
-
 import React from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 
 const Nav = () => {
-
-
-
   const navigate = useNavigate();
   const auth = localStorage.getItem("user")
 
@@ -15,23 +11,18 @@ const Nav = () => {
   }
   return (
     <div className='nav'>
-
       {auth ?
         <ul>
           <li><Link to="/">Product</Link></li>
           <li><Link to="/add">add Product</Link></li>
           <li><Link to="/Profile">My Profile</Link></li>
-
           <li><Link onClick={logout} to="/login">Logout ({JSON.parse(auth).name})</Link></li>  </ul> :
-
         <ul>
           <li><Link to="/login">login</Link></li>
           <li><Link to="/signup">signup</Link></li>
         </ul>
       }
-
     </div>
   )
 }
-
 export default Nav
